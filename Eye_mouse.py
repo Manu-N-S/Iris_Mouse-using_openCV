cd36335d875dc3daf375e3aa7bae52b20c11e095
MANU N S <98375679+Manu-N-S@users.noreply.github.com>
2023-03-28 20:24:03 +0530

    Update Eye_mouse.py

--- Eye_mouse.py
+++ Eye_mouse.py
@@ -1,3 +1,9 @@
+# Copyright (c) <2023>, <MANU N S>
+# All rights reserved.
+
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. 
+
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
